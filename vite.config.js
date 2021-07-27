fbbacf5cb24f802985e0f9e639a6853d32501616
DEK-Pham-The-Anh <anh.pham@dek-cz.com>
2021-07-27 12:44:35 +0200

    n: pre-bundle ops f.e. renaming scripts

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import glob from 'glob';
 
 const empty = require('empty-folder');
 
+const fs = require('fs');
+
 const { createVuePlugin } = require('vite-plugin-vue2');
 
 const vueSrcDir = "www/vue-development/src";
@@ -19,19 +21,46 @@ const reload = {
     }
 }
 
-function emptyVueSrcDir() {
+function customBuildPlugin() {
     return {
         buildStart() {
             let dir = path.resolve(__dirname, vueSrcDir);
             empty(dir, false, () => { 
                 console.log('\nFolder ' + dir + ' emptied.'); 
             });
+        },
+        generateBundle(options, bundle) {
+            // let manifestVue = path.resolve(__dirname, vueSrcDir, "manifest.json"); console.log('manifestVue', manifestVue);
+
+            // fs.readFile('/www/vue-development/src/manifest.json', function(err, data) {
+            //     console.log(data);
+            // });
+
+            console.log('\nBundle contains ' + Object.keys(bundle).length + ' file(s).');
+            
+            for (const key in bundle) {
+                let item = bundle[key]; 
+
+                if (item.type == 'chunk' && item.facadeModuleId) {  // scripts
+
+                    let facadeModuleIdArr = item.facadeModuleId.split('/');
+                    let appName = facadeModuleIdArr[facadeModuleIdArr.length - 2];
+
+                    item.fileName = appName + '.min.js';
+                
+                } else if (item.type == 'asset') {    // stylesheets
+                    
+                    // This is a problem here
+
+                }
+            }
+
         }
     };
 }
 
 export default {
-    plugins: [reload, createVuePlugin(), emptyVueSrcDir()],
+    plugins: [reload, createVuePlugin(), customBuildPlugin()],
     server: {
         watch: {
             usePolling: true
@@ -45,7 +74,8 @@ export default {
         rollupOptions: {
             input:  glob.sync(path.resolve(__dirname, "www/vue-development/app/*/", "index.js")),
             output: {
-                dir: vueSrcDir
+                dir: vueSrcDir,
+                chunkFileNames: "[name]-[hash].js" 
             }
         }
     }
